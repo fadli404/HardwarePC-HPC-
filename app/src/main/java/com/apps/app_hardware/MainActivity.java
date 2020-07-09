088a18d9769a640b7e59b9c7c1431203fe174619
fadli404 <lifad14@gmail.com>
2020-07-09 12:49:23 +0700

    Fix Error

--- app/src/main/java/com/apps/app_hardware/MainActivity.java
+++ app/src/main/java/com/apps/app_hardware/MainActivity.java
@@ -2,6 +2,7 @@ package com.apps.app_hardware;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(2500);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(MainActivity.this, Tutor.class));
+                }
+            }
+        };
+        thread.start();
     }
 }
--- app/src/main/java/com/apps/app_hardware/Tutor.java
+++ app/src/main/java/com/apps/app_hardware/Tutor.java
@@ -1,14 +1,114 @@
 package com.apps.app_hardware;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tutor extends AppCompatActivity {
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnMulai;
+    Animation btnAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
+        if (restorePrefData()){
+            Intent menuutama = new Intent(getApplicationContext(),MenuUtama.class);
+            startActivity(menuutama);
+            finish();
+        }
+
+        btnNext = findViewById(R.id.tmbl_lanjut);
+        btnMulai = findViewById(R.id.tmbl_mulai);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
+
+        final List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("CONTOH1","WKWKWKWKWKWKWKWKWKWKWKWKKWWKWK",R.drawable.profile_putih));
+        mList.add(new ScreenItem("CONTOH2","HEHEHEHEHEHEHEHEHEHEHEHEHEHEHE",R.drawable.music_putih));
+        mList.add(new ScreenItem("Semoga Bermanfaat","#DirumahAja",R.drawable.ceklis));
+
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        tabIndicator.setupWithViewPager(screenPager);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()){
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+                if (position == mList.size()-1){
+                    loadLastScreen();
+                }
+            }
+        });
+        tabIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == mList.size()-1){
+                    loadLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        btnMulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menuutama = new Intent(getApplicationContext(),MenuUtama.class);
+                startActivity(menuutama);
+                savePrefsData();
+                finish();
+            }
+        });
+    }
+
+    private boolean restorePrefData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        Boolean isIntroOpenedBefore = pref.getBoolean("isIntroOpened",false);
+        return isIntroOpenedBefore;
+    }
+
+    private void savePrefsData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened",true);
+        editor.commit();
+    }
+
+    private void loadLastScreen() {
+        btnNext.setVisibility(View.INVISIBLE);
+        btnMulai.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        btnMulai.setAnimation(btnAnim);
     }
 }
